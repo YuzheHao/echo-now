1d9cb720bc79b297b70f0d1f132ead8e7825992e
Tetsuya <me.ice.cream@github.com>
2020-05-14 13:56:45 +0900

    Initial Commit

---
+++ echo-now/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  echo-now
+//
+//  Created by 郝 宇哲 on 2020/05/14.
+//  Copyright © 2020 郝 宇哲. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
